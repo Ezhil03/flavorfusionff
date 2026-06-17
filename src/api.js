f5546698213931ae95b8a88892ccb5a52dffbbf9
EZHILARASAN <ezhilarasanj2005@gmail.com>
2026-06-17 14:17:57 +0530

    Add files via upload

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:5000/api" });
+const api = axios.create({ baseURL: "https://flavorfusion-2-an27.onrender.com/api" });
+
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
