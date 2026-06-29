bf924a34628722bde23a25ff55b5c8ee20585a98
EZHILARASAN <ezhilarasanj2005@gmail.com>
2026-06-29 12:06:34 +0530

    Add files via upload

--- src/api/api.js
+++ src/api/api.js
@@ -7,7 +7,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "http://localhost:5000/api",
+    "https://flavorfusion-new.onrender.com/api",
   withCredentials: false,
 });
 
